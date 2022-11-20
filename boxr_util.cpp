7d7a6e61454217152f9a95593be8af0403360068
Sidien <sidien.mq2@gmail.com>
2022-11-20 12:18:08 +0100

    fixup! fixup! Add Boxr TLO

--- boxr_util.cpp
+++ boxr_util.cpp
@@ -6,7 +6,7 @@ bool EvaluateBooleanMacroExpression(const std::string& expression) {
 	CHAR evaluatedExpression[MAX_STRING];
 	strcpy_s(evaluatedExpression, expression.c_str());
 	ParseMacroData(evaluatedExpression, MAX_STRING);
-	double CalculateResult = 0;
-	Calculate(evaluatedExpression, CalculateResult);
-	return CalculateResult != 0;
+	double calculateResult = 0;
+	Calculate(evaluatedExpression, calculateResult);
+	return calculateResult != 0;
 }
