78fd3e6adcab9e0a42442a7ca43803e7459807a9
Sidien <sidien.mq2@gmail.com>
2022-12-15 17:14:33 +0100

    Update Boxr to build for EMU

--- boxr_util.cpp
+++ boxr_util.cpp
@@ -1,3 +1,4 @@
+#include "boxr.h"
 #include "boxr_util.h"
 #include "boxr_logger.h"
 #include <mq/Plugin.h>
@@ -9,4 +10,12 @@ bool EvaluateBooleanMacroExpression(const std::string& expression) {
 	double calculateResult = 0;
 	Calculate(evaluatedExpression, calculateResult);
 	return calculateResult != 0;
+}
+
+char* GetRaidMainAssistName(int raidAssistNum) {
+#if defined(ROF2EMU) || defined(UFEMU)
+	throw UnsupportedBoxrOperationException("Boxr does not support Raid MA operations on EMU");
+#else
+	return GetCharInfo()->raidData.MainAssistNames[raidAssistNum - 1];
+#endif
 }
